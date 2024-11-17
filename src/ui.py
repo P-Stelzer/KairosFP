13cb0161e3508b8e805e50e24b246a0acc3b5391
Peter Stelzer <pstelzer2023@my.fit.edu>
2024-11-17 16:30:13 -0500

    Days now are associated with their dates

--- src/ui.py
+++ src/ui.py
@@ -3,28 +3,39 @@ from PySide6.QtWidgets import (
     QPushButton,
     QScrollArea,
     QVBoxLayout,
-    QWidget,
+    QFrame,
 )
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QTimer
+from datetime import date as Date, timedelta
+
+CURRENT_YEAR, CURRENT_WEEK, _ = Date.today().isocalendar()
+FIRST_DAY_OF_CURRENT_WEEK = Date.fromisocalendar(
+    CURRENT_YEAR, CURRENT_WEEK, 1
+) - timedelta(days=1)  # -1 days to make it sunday
+UNIT_WEEK = timedelta(weeks=1)
+
+
+class Day(QPushButton):
+    def __init__(self, date: Date) -> None:
+        super().__init__()
+
+        self.date = date
+        color = "blue" if date == Date.today() else "black"
+        self.setStyleSheet(f"border-radius : 0; border : 2px solid {color}")
+        self.setMinimumSize(100, 100)
+        self.setText(f"{self.date.year}/{self.date.month}/{self.date.day}")
 
 
 class Week(QHBoxLayout):
-    def __init__(self, n) -> None:
+    def __init__(self, first_day_of_week: Date) -> None:
         super().__init__()
 
-        # self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        # day_row_layout = QHBoxLayout()
-        # day_row_layout.setSpacing(0)
-        # day_row_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        # self.setLayout(day_row_layout)
         self.setSpacing(0)
         self.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        for _ in range(7):
-            day_button = QPushButton(str(n))
-            day_button.setStyleSheet("border-radius : 0; border : 2px solid black")
-            day_button.setMinimumSize(100, 100)
-            # day_row_layout.addWidget(day_button)
+        for i in range(7):
+            day = first_day_of_week + timedelta(days=i)
+            day_button = Day(day)
             self.addWidget(day_button)
 
 
@@ -34,55 +45,54 @@ class InfiniteScrollArea(QScrollArea):
 
         self.setWidgetResizable(True)
 
-        area_widget = QWidget()
-        # area_widget.setSizePolicy(
-        #     QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding
-        # )
+        area_widget = QFrame(self)
         self.area_layout = QVBoxLayout()
         self.area_layout.setSpacing(0)
         self.area_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         area_widget.setLayout(self.area_layout)
-        # area_widget.layout().addWidget(QPushButton("test"))
-        # self.area_layout.addStretch()
-        # area_widget.setMinimumHeight(500)
 
         self.setWidget(area_widget)
 
-        self.min = 0
-        self.max = 0
+        self.min = 0  # Number of weeks before current week
+        self.max = 0  # Number of weeks after current week
 
+        # Check scrollbar position on a timer
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.scrolled)
+        self.timer.start(50)
+
+        # Check scrollbar position when it changes
+        # self.verticalScrollBar().valueChanged.connect(self.scrolled)
+
+        # Correct scrollbar position after upward extension
         self.slider_max = self.verticalScrollBar().maximum()
-
-        self.verticalScrollBar().valueChanged.connect(self.scrolled)
         self.verticalScrollBar().rangeChanged.connect(self.correct_slider)
-        # self.verticalScrollBar().setVisible(False)
+
+        # Do not show a scrollbar
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
+        # Add initial content
         self.extend_upwards(5)
         self.extend_downwards(10)
-        # self.area_layout.addStretch()
-        # self.setWidget(area_widget)
-        self.show()
 
-    def scrolled(self, value):
+    def scrolled(self):
+        value = self.verticalScrollBar().value()
         if value == self.verticalScrollBar().maximum():
             self.extend_downwards(5)
         elif value == self.verticalScrollBar().minimum():
             self.extend_upwards(5)
-            # self.verticalScrollBar().setSliderPosition(150)
 
     def extend_downwards(self, n):
         for _ in range(n):
-            # self.area_layout.addWidget(QPushButton(str(self.max)))
-            self.area_layout.addLayout(Week(self.max))
+            new_week = FIRST_DAY_OF_CURRENT_WEEK + (self.max * UNIT_WEEK)
+            self.area_layout.addLayout(Week(new_week))
             self.max += 1
-            # self.setWidget(self.widget())
 
     def extend_upwards(self, n):
         for _ in range(n):
             self.min -= 1
-            self.area_layout.insertLayout(0, Week(self.min))
-            # self.area_layout.insertWidget(0, QPushButton(str(self.min)))
+            new_week = FIRST_DAY_OF_CURRENT_WEEK + (self.min * UNIT_WEEK)
+            self.area_layout.insertLayout(0, Week(new_week))
 
     def correct_slider(self, min, max):
         if self.verticalScrollBar().value() == min:
