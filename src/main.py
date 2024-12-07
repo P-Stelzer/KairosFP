f679084c711184913271913c0f55d51cdae2ccae
prismaticr <bubbgian@gmail.com>
2024-12-07 00:31:40 -0500

    Added Tag Editor

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import sys
+from db import Tag
 from PySide6.QtWidgets import QApplication
 
 from kui.calendar import InfiniteScrollArea
+from kui.tag_editor import TagEditor
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     window = InfiniteScrollArea()
     window.show()
+    tag_editor = TagEditor(Tag(-1, "testName", "testDesc"))
+    tag_editor.show()
 
     app.exec()
